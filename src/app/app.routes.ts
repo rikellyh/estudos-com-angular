b34072f3f6e2a2a5ac3dc35da9e02625a7d93a0a
rikellyh <rikellyh898@gmail.com>
2024-02-18 23:32:25 -0300

    update assets: fonts, design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,4 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
-export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'dashboard', component: DashboardComponent },
-];
+export const routes: Routes = [{ path: "", component: HomeComponent }];
