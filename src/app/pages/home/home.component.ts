4e262ab001791471363e62a45a680373fb823c8d
rikellyh <rikellyh898@gmail.com>
2024-02-26 13:13:57 -0300

    finish search country

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,18 @@
-import { Component, HostListener, OnInit, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { HttpErrorResponse } from '@angular/common/http';
+import { Component, HostListener, OnInit, inject } from "@angular/core";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { HttpErrorResponse } from "@angular/common/http";
 
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { MatSelectModule } from "@angular/material/select";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { NgxPaginationModule } from "ngx-pagination";
 
-import { Country } from '../../types/units-response.interface';
-import { GetUnitsService } from './../../services/get-units.service';
-import { StyleManagerService } from '../../services/style-manager.service';
+import { Country } from "../../types/units-response.interface";
+import { GetUnitsService } from "./../../services/get-units.service";
+import { StyleManagerService } from "../../services/style-manager.service";
 
 @Component({
-  selector: 'app-home',
+  selector: "app-home",
   standalone: true,
   imports: [
     RouterLink,
@@ -22,11 +22,14 @@ import { StyleManagerService } from '../../services/style-manager.service';
     MatButtonModule,
     NgxPaginationModule,
   ],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css',
+  templateUrl: "./home.component.html",
+  styleUrl: "./home.component.css",
 })
 export class HomeComponent implements OnInit {
   public countries: Country[] = [];
+  public filteredCountries: Country[] = [];
+  public uniqueRegions: string[] = [];
+  public selectedRegion: string | null = null;
   page = 1;
   count = 0;
   pageSize = 8;
@@ -42,13 +45,42 @@ export class HomeComponent implements OnInit {
     this.unitService.getCountries().subscribe(
       (response: Country[]) => {
         this.countries = response;
-        this.count = this.countries.length;
+        this.filteredCountries = response;
+        this.count = this.filteredCountries.length;
+        this.uniqueRegions = Array.from(
+          new Set(this.countries.map((country) => country.region))
+        );
       },
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
     );
   }
+
+  searchCountry(country: string) {
+    country = country ? country.trim() : "";
+    this.filteredCountries = this.countries.filter((item) => {
+      return Object.values(item)
+        .join("")
+        .trim()
+        .toLowerCase()
+        .includes(country.trim().toLowerCase());
+    });
+
+    this.count = this.filteredCountries.length;
+  }
+
+  // filter(array: Array) {
+  //   const selected = array.filter((item) =>
+  //     this.selectedRegion.includes(item.region)
+  //   );
+
+  //   const notSelected = array.filter(
+  //     (item) => !this.selectedRegion.includes(item.region)
+  //   );
+
+  //   return [...selected, ...notSelected];
+  // }
 
   handlePageChange(event: number): void {
     this.page = event;
@@ -57,7 +89,7 @@ export class HomeComponent implements OnInit {
   }
 
   scrollToTop(): void {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
   private styleManager = inject(StyleManagerService);
@@ -65,18 +97,18 @@ export class HomeComponent implements OnInit {
 
   inside = false;
   touched =
-    '1px solid var(--mdc-outlined-text-field-disabled-input-text-color)';
+    "1px solid var(--mdc-outlined-text-field-disabled-input-text-color)";
 
   onClick() {
-    this.touched = '1px solid var(--mat-badge-background-color)';
+    this.touched = "1px solid var(--mat-badge-background-color)";
     this.inside = true;
   }
 
-  @HostListener('document:click')
+  @HostListener("document:click")
   clickedOutside() {
     if (!this.inside) {
       this.touched =
-        '1px solid var(--mdc-outlined-text-field-disabled-input-text-color)';
+        "1px solid var(--mdc-outlined-text-field-disabled-input-text-color)";
     }
     this.inside = false;
   }
--- src/app/services/filter-units.service.ts
+++ src/app/services/filter-units.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Country } from "../types/units-response.interface";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class FilterUnitsService {
+  constructor() {}
 
-  constructor() { }
+  // onRegionSelect(country: Country[]){
+  //   let result = country;
+  //   let selectedRegion = result;
+
+  //   const selected = country.filter((item) =>
+  //   selectedRegion.includes(item.region)
+  // );
+
+  // const notSelected = country.filter(
+  //   (item) => !selectedRegion.includes(item.region)
+  // );
+
+  // return [...selected, ...notSelected];
+  // }
 }
